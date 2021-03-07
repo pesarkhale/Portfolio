60350adccb93e25929d704582c18d246aed46b19
Pouya Babaie <pouyababaie123@gmail.com>
2021-03-07 20:12:30 +0330

    Some JS added

---
+++ main.js
@@ -0,0 +1,13 @@
+const Home={
+doorOpen : false,
+address : "Karaj",
+TVon: true,
+ferniture : ["TV","Bokhari","Doors",""],
+Kitchen:{
+    Dooropen : true,
+    KitchenFanOn : false,
+     
+}
+
+
+};
